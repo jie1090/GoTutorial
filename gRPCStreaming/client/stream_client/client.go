db8df49e4ed7e4ad97df465c6974f511faef8d3f
yangjie <1187926166@qq.com>
2022-03-04 11:13:08 +0800

    gRPCStreaming

---
+++ gRPCStreaming/client/stream_client/client.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+
+	pb "github.com/jie1090/GoTutorial/gRPCStreaming/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	PORT = "9002"
+)
+
+func main() {
+	conn, err := grpc.Dial(":"+PORT, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("grpc.Dial err: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewStreamServiceClient(conn)
+
+	err = printLists(client, &pb.StreamRequest{Pt: &pb.StreamPoint{Name: "gRPC Stream Client: List", Value: 2018}})
+	if err != nil {
+		log.Fatalf("printLists.err: %v", err)
+	}
+
+	err = printRecord(client, &pb.StreamRequest{Pt: &pb.StreamPoint{Name: "gRPC Stream Client: Record", Value: 2018}})
+	if err != nil {
+		log.Fatalf("printRecord.err: %v", err)
+	}
+
+	err = printRoute(client, &pb.StreamRequest{Pt: &pb.StreamPoint{Name: "gRPC Stream Client: Route", Value: 2018}})
+	if err != nil {
+		log.Fatalf("printRoute.err: %v", err)
+	}
+}
+
+func printLists(client pb.StreamServiceClient, r *pb.StreamRequest) error {
+	stream, err := client.List(context.Background(), r)
+	if err != nil {
+		return err
+	}
+
+	for {
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		log.Printf("resp: pt.name: %s, pt.value: %d", resp.Pt.Name, resp.Pt.Value)
+	}
+
+	return nil
+}
+func printRecord(client pb.StreamServiceClient, r *pb.StreamRequest) error {
+	return nil
+}
+func printRoute(client pb.StreamServiceClient, r *pb.StreamRequest) error {
+	return nil
+}
---
+++ gRPCStreaming/server/stream_server/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/jie1090/GoTutorial/gRPCStreaming/proto"
+	"google.golang.org/grpc"
+)
+
+type StreamService struct{}
+
+const (
+	PORT = "9002"
+)
+
+func main() {
+	server := grpc.NewServer()
+	pb.RegisterStreamServiceServer(server, &StreamService{})
+
+	lis, err := net.Listen("tcp", ":"+PORT)
+	if err != nil {
+		log.Fatal("net.Listen err: %v", err)
+	}
+
+	server.Serve(lis)
+}
+
+func (s *StreamService) List(r *pb.StreamRequest, stream pb.StreamService_ListServer) error {
+	for n := 0; n <= 6; n++ {
+		err := stream.Send(&pb.StreamResponse{
+			Pt: &pb.StreamPoint{
+				Name:  r.Pt.Name,
+				Value: r.Pt.Value + int32(n),
+			},
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *StreamService) Record(stream pb.StreamService_RecordServer) error {
+	return nil
+}
+
+func (s *StreamService) Route(stream pb.StreamService_RouteServer) error {
+	return nil
+}
